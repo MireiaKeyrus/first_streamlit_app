dac2a4f1df465c6c3fc9e1588a5db4f13f5e5cca
MireiaKeyrus <55284997+MireiaKeyrus@users.noreply.github.com>
2022-05-18 15:43:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('Nouveau Resto')
 
-streamlit.header('Breakfast Menu')
+streamlit.header(🥣 'Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
